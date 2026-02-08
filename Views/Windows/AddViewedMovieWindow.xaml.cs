743abd41029c97c60afc1cda86b15cdf203007b6
EnderEzhik <Achernov2007@gmail.com>
2026-02-09 01:04:39 +0300

    Обновлены окна под новые модели данных

--- Views/Windows/AddViewedMovieWindow.xaml.cs
+++ Views/Windows/AddViewedMovieWindow.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System.Windows;
 using ChinaMan.Database;
-using ChinaMan.Models;
+using ChinaMan.Database.Models;
 using ChinaMan.ViewModels;
 
 namespace ChinaMan
@@ -13,29 +13,6 @@ namespace ChinaMan
         public AddViewedMovieWindow()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            string title = this.MovieTitleInput.Text;
-            string description = this.MovieDescriptionInput.Text;
-            string rating = this.MovieRatingInput.Text;
-            DateTime viewedDate = this.MovieViewedDate.SelectedDate.HasValue ? this.MovieViewedDate.SelectedDate.Value : DateTime.Now;
-            viewedDate = viewedDate.Date;
-
-            ViewedMovie movieData = new ViewedMovie();
-            movieData.Title = title;
-            movieData.Description = description;
-            movieData.Rating = int.Parse(rating);
-            movieData.ViewedDate = viewedDate;
-
-            using (var dbContext = App.CreateDbContext())
-            {
-                DatabaseService.SaveViewedMovie(dbContext, movieData);
-            }
-
-            MainViewModel.Instance.ViewedMovies.Add(movieData);
-            this.Close();
         }
     }
 }
--- Views/Windows/MainWindow.xaml.cs
+++ Views/Windows/MainWindow.xaml.cs
@@ -1,7 +1,7 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
 using ChinaMan.Database;
-using ChinaMan.Models;
+using ChinaMan.Database.Models;
 using ChinaMan.ViewModels;
 
 namespace ChinaMan
@@ -26,22 +26,6 @@ namespace ChinaMan
 
         private void ListView_Loaded(object sender, RoutedEventArgs e)
         {
-            var viewModel = (MainViewModel)this.DataContext;
-            var movies = dbContext.viewedMovies.ToList();
-
-            foreach (var movie in movies)
-            {
-                viewModel.ViewedMovies.Add(movie);
-            }
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            var btnContext = (ViewedMovie)((Button)sender).DataContext;
-            var viewModel = (MainViewModel)this.DataContext;
-            viewModel.ViewedMovies.Remove(btnContext);
-            dbContext.viewedMovies.Remove(btnContext);
-            dbContext.SaveChanges();
         }
     }
 }
