c2078d5c5c7130ef165c558b72bc0a9e63a30ca3
EnderEzhik <Achernov2007@gmail.com>
2026-03-15 23:10:09 +0300

    add: добавлена основная ViewModel для хранения состояния главного окна и вкладок

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+using ChinaMan.ViewModels.Base;
+
+namespace ChinaMan.ViewModels
+{
+    internal class MainViewModel : BaseViewModel
+    {
+        private TabItem _currentTabItem;
+        public TabItem CurrentTabItem
+        {
+            get => _currentTabItem;
+            set => Set(ref _currentTabItem, value);
+        }
+    }
+}
