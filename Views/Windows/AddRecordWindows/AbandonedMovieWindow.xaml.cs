18a30335fd4c9eec388cbf7aee2d8c8a43d11f32
EnderEzhik <Achernov2007@gmail.com>
2026-03-16 13:02:40 +0300

    add: добавлены окна для добавления записей в разные списки

---
+++ Views/Windows/AddRecordWindows/AbandonedMovieWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ChinaMan.Views.Windows.AddRecordWindows
+{
+    /// <summary>
+    /// Логика взаимодействия для AbandonedMovieWindow.xaml
+    /// </summary>
+    public partial class AbandonedMovieWindow : Window
+    {
+        public AbandonedMovieWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Views/Windows/AddRecordWindows/WantToViewMovieWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ChinaMan.Views.Windows.AddRecordWindows
+{
+    /// <summary>
+    /// Логика взаимодействия для WantToViewMovieWindow.xaml
+    /// </summary>
+    public partial class WantToViewMovieWindow : Window
+    {
+        public WantToViewMovieWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
