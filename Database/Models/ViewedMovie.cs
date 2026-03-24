f8e39981cc38a0f15340ca6de131d0768b4c3c46
EnderEzhik <Achernov2007@gmail.com>
2026-03-24 21:21:43 +0300

    add: добавлены таблицы в базе данных для всех типов записей. change: таблица для записей о просмотренных фильмах переименована.
    !ВНИМАНИЕ! Миграция приведет к потере данных из таблицы Views.

---
+++ Database/Models/ViewedMovie.cs
@@ -0,0 +1,12 @@
+﻿namespace ChinaMan.Database.Models
+{
+    internal class ViewedMovie
+    {
+        public int Id { get; set; }
+        public DateTime ViewedDate { get; set; } = DateTime.Now;
+        public int Rating { get; set; }
+
+        public int FilmId { get; set; }
+        public Film Film { get; set; }
+    }
+}
--- Database/Models/WatchingMovie.cs
+++ Database/Models/WatchingMovie.cs
@@ -1,12 +1,9 @@
 ﻿namespace ChinaMan.Database.Models
 {
-    internal class View
+    internal class WatchingMovie
     {
         public int Id { get; set; }
         public int FilmId { get; set; }
-        public DateTime ViewedDate { get; set; } = DateTime.Now;
-        public int Rating { get; set; }
-
         public Film Film { get; set; }
     }
 }
